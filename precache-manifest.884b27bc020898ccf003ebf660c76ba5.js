8fe553fdc1a34e777cfda788158e732d944e0d4b
ayshaeman <ayshaemanbda@gmail.com>
2019-01-14 16:14:14 -0500

    Updates

---
+++ precache-manifest.884b27bc020898ccf003ebf660c76ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8494005c3b72751a3c77",
+    "url": "/resume-/static/js/runtime~main.8494005c.js"
+  },
+  {
+    "revision": "2bd5978c1ceea45ae522",
+    "url": "/resume-/static/js/main.2bd5978c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resume-/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2bd5978c1ceea45ae522",
+    "url": "/resume-/static/css/main.31e28687.chunk.css"
+  },
+  {
+    "revision": "975cbce50697dbcf5e27dc10d4847aa6",
+    "url": "/resume-/index.html"
+  }
+];
